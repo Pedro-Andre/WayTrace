e552fe84c1ab52397d5353903f87af665df88e53
Pedro-Andre <pedro.andre.nv@gmail.com>
2026-04-26 16:10:48 -0300

    Initializing CRUD

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,13 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+import userRoutes from "./routes/user-routes";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
+app.use("/users", userRoutes);
 
 app.post("/location", (req: Request, res: Response) => {
   const { latitude, longitude } = req.body;
